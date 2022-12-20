0d7185c85e1bb3c8d6bb5ccf053fcd04ab05f219
nicolaiokkels <nico902t@stud.kea.dk>
2022-12-20 18:55:50 +0100

    changed port

--- server/index.js
+++ server/index.js
@@ -16,9 +16,7 @@ app.use(cors());
 
 app.use('/tasks', taskRoutes);
 app.use('/users', userRoutes);
-const PORT = process.env.PORT || 5000;
-
-//test
+const PORT = process.env.PORT || 5500;
 
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
